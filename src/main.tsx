98b28b626667725da74e45a1ecedd2c2e069c662
sam <sanskarsharma9005@gmail.com>
2024-12-03 20:38:36 +0530

    fix the vercel error

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { registerSW } from 'virtual:pwa-register';
-import AppCreateButton from './components/addApp';
 
 registerSW();
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AppCreateButton /> 
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
 )
